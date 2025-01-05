277e0ccb1b179519bef5964620f00a3700054781
jsaidler <jsaidler@gmail.com>
2025-01-05 16:34:00 -0300

    Update view_plugin.py

--- pibooth/plugins/view_plugin.py
+++ pibooth/plugins/view_plugin.py
@@ -162,8 +162,9 @@ class ViewPlugin(object):
     @pibooth.hookimpl
     def state_print_validate(self, app, win, events):
         printed = app.find_print_event(events)
-        self.forgotten = app.find_capture_event(events)
-        if self.print_view_timer.is_timeout() or printed or self.forgotten:
+        capture_pressed = app.find_capture_event(events)
+	    self.forgotten = capture_pressed and app.can_forget
+	    if self.print_view_timer.is_timeout() or printed or capture_pressed:
             if printed:
                 win.set_print_number(len(app.printer.get_all_tasks()), not app.printer.is_ready())
             return 'finish'
