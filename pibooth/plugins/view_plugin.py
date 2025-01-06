c84a2d994238e0f9c8b8f48a6ae348e6c933d75b
jsaidler <jsaidler@gmail.com>
2025-01-05 21:19:43 -0300

    Update view_plugin.py

--- pibooth/plugins/view_plugin.py
+++ pibooth/plugins/view_plugin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pibooth
+import pygame
 from pibooth.utils import LOGGER, get_crash_message, PoolingTimer
 
 
@@ -124,17 +125,17 @@ class ViewPlugin(object):
         self.count += 1
         win.set_capture_number(self.count, app.capture_nbr)
 
-    # @pibooth.hookimpl
-    # def state_preview_validate(self, app, events):
-    #     # event = app.find_capture_event(events)
-    #     # if event:
-    #     #     return 'capture'
-    #     event = app.find_choice_event(events)
-    #     if event:
-    #         if event.key == pygame.K_LEFT:
-    #             'chosen'
-    #         elif event.key == pygame.K_RIGHT:
-    #             'capture'
+    @pibooth.hookimpl
+    def state_preview_validate(self, app, events):
+        # event = app.find_capture_event(events)
+        # if event:
+        #     return 'capture'
+        event = app.find_choice_event(events)
+        if event:
+            if event.key == pygame.K_LEFT:
+                'chosen'
+            elif event.key == pygame.K_RIGHT:
+                'capture'
 
     @pibooth.hookimpl
     def state_capture_do(self, app, win):
