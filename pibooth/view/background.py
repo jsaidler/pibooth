e0ec3d06a5bf8720df9bccb1ea08b18ff191560a
jsaidler <jsaidler@gmail.com>
2025-01-07 18:17:48 -0300

    Update background.py

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -211,9 +211,9 @@ class IntroBackground(Background):
             self.camera_icon = pictures.get_pygame_image("camera.png",  self.camera_icon_size, vflip=False, color=self._text_color)    
             self.camera_icon_pos = (int(self._rect.width * 0.03), int(self._rect.height *0.2))
             
-            self.logo_size = (self._rect.width, self._rect.height)
+            self.logo_size = (self._rect.width * 0.85, self._rect.height * 0.85)
             self.logo = pictures.get_pygame_image("logo.png",  self.logo_size, vflip=False, color=None, crop=True)    
-            self.logo_pos = (int(self._rect.width * 0.53), int(self._rect.height *0.2))
+            self.logo_pos = (int(self._rect.width * 0.53), 0)
 
             
             self.exit_icon_size = (self._rect.width * 0.15, self._rect.height * 0.15)
