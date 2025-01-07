56bd36fcf48c3ed114a914050ce27502270f9190
jsaidler <jsaidler@gmail.com>
2025-01-07 17:15:19 -0300

    Update background.py

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -211,7 +211,7 @@ class IntroBackground(Background):
             
             self.exit_icon_size = (self._rect.width * 0.15, self._rect.height * 0.15)
             self.exit_icon = pictures.get_pygame_image("exit.png",  self.exit_icon_size, vflip=False, color=self._text_color)    
-            self.exit_icon_pos = (int(self._rect.width * 0.01), int(self._rect.width *0.01))
+            self.exit_icon_pos = (int(self._rect.width * 0.01), int(self._rect.width *0.1))
 
     def paint(self, screen):
         Background.paint(self, screen)
