8f91ad3268c0d03e8ee086c8c24684e3fded0487
jsaidler <jsaidler@gmail.com>
2025-01-07 15:20:03 -0300

    Update window.py

--- pibooth/view/window.py
+++ pibooth/view/window.py
@@ -37,16 +37,12 @@ class PiWindow(object):
                  size=(800, 480),
                  color=(0, 0, 0),
                  text_color=(255, 255, 255),
-                 arrow_location=None,
-                 arrow_offset=0,
                  can_forget=True,
                  debug=False):
         self.__size = size
         self.debug = debug
         self.bg_color = color
         self.text_color = text_color
-        self.arrow_location = arrow_location
-        self.arrow_offset = arrow_offset
         self.can_forget = can_forget
 
         # Prepare the pygame module for use
@@ -251,9 +247,9 @@ class PiWindow(object):
         """
         self._capture_number = (0, self._capture_number[1])
         if with_print and pil_image:
-            self._update_background(background.IntroWithPrintBackground(self.arrow_location, self.arrow_offset))
+            self._update_background(background.IntroWithPrintBackground())
         else:
-            self._update_background(background.IntroBackground(self.arrow_location, self.arrow_offset))
+            self._update_background(background.IntroBackground())
 
         if pil_image:
             self._update_foreground(pil_image, self.RIGHT)
@@ -266,7 +262,7 @@ class PiWindow(object):
         """
         self._capture_number = (0, self._capture_number[1])
         if not selected:
-            self._update_background(background.ChooseBackground(choices, self.arrow_location, self.arrow_offset))
+            self._update_background(background.ChooseBackground(choices, ))
         else:
             self._update_background(background.ChosenBackground(choices, selected))
 
@@ -294,9 +290,7 @@ class PiWindow(object):
         """Show print view (image resized on the left).
         """
         self._capture_number = (0, self._capture_number[1])
-        self._update_background(background.PrintBackground(self.arrow_location,
-                                                           self.arrow_offset,
-                                                           self.can_forget))
+        self._update_background(background.PrintBackground(self.can_forget))
         if pil_image:
             self._update_foreground(pil_image, self.CENTER)
 
