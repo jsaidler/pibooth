dac0c28aaa86b5e79d8f0d5b23afa9e3e3729b12
jsaidler <jsaidler@gmail.com>
2025-01-07 22:43:42 -0300

    Update background.py

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -425,7 +425,7 @@ class ProcessingBackground(Background):
                            int(self._rect.height * 0.82 - self._rect.width * 0.01),
                            int(self._rect.width * 0.5),
                            int(self._rect.height * 0.18))
-        Background.resize_texts(self, rect, align='left')
+        Background.resize_texts(self, rect, align='bottom-left')
 
 
 class PrintBackground(Background):
