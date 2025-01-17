775e0ed2728c7a0ac4e34e428573b83ef16d40fd
jsaidler <jsaidler@gmail.com>
2025-01-17 12:31:07 -0300

    Update rpi.py

--- pibooth/camera/rpi.py
+++ pibooth/camera/rpi.py
@@ -85,8 +85,8 @@ class RpiCamera(BaseCamera):
         else:
             index = np.absolute(self._shutter_values - self._cam.exposure_speed).argmin()
         speed = self._shutter_values[index]
-        self._cam.shutter_speed = 1/speed
-        LOGGER.info("Current shutter speed is 1/%s", 1//speed)
+        self._cam.shutter_speed = 1000000//speed
+        LOGGER.info("Current shutter speed is 1/%s", 1000000//speed)
         return (index, self._cam.shutter_speed)
     
     def set_auto_shutter(self):
