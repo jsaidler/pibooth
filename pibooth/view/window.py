6c11a7015de7bf22bee345040dd30092d0d4bebc
jsaidler <jsaidler@gmail.com>
2025-01-18 00:02:24 -0300

    Update window.py

--- pibooth/view/window.py
+++ pibooth/view/window.py
@@ -254,6 +254,7 @@ class PiWindow(object):
             self._update_background(background.IntroWithPrintBackground())
         else:
             self._update_background(background.IntroBackground())
+            self.set_print_number(len(app.printer.get_all_tasks()), app.printer.is_ready())
 
         if pil_image:
             self._update_foreground(pil_image, self.RIGHT)
@@ -337,22 +338,22 @@ class PiWindow(object):
     def set_print_number(self, current_nbr=None, ready=None):
         """Set the current number of tasks in the printer queue.
         """
-        update = False
+        # update = False
 
-        if current_nbr is not None and self._print_number != current_nbr:
-            self._print_number = current_nbr
-            update = True
+        # if current_nbr is not None and self._print_number != current_nbr:
+        #     self._print_number = current_nbr
+        #     update = True
 
-        if ready is not None and self._print_failure != (not ready):
-            self._print_failure = not ready
-            update = True
+        # if ready is not None and self._print_failure != (not ready):
+        #     self._print_failure = not ready
+        #     update = True
 
-        if update:
-            self._update_background(self._current_background)
-            if self._current_foreground:
-                self._update_foreground(*self._current_foreground)
-            self._update_print_number()
-            pygame.display.update()
+        # if update:
+        #     self._update_background(self._current_background)
+        #     if self._current_foreground:
+        #         self._update_foreground(*self._current_foreground)
+        self._update_print_number()
+        pygame.display.update()
 
     def toggle_fullscreen(self):
         """Set window to full screen or initial size.
