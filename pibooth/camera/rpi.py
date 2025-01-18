23ae9eaefca69a1e4645beb13b44cc8d9089ab68
jsaidler <jsaidler@gmail.com>
2025-01-18 14:33:34 -0300

    Update rpi.py

--- pibooth/camera/rpi.py
+++ pibooth/camera/rpi.py
@@ -140,7 +140,7 @@ class RpiCamera(BaseCamera):
         border_thickness = 15
         borders = pygame.Surface((tuple(rect)[2] + border_thickness * 2,tuple(rect)[3] + border_thickness * 2), pygame.SRCALPHA, 32)
         pygame.draw.rect(borders, pygame.Color(255,255,255), borders.get_rect(), border_thickness)
-        self._window.surface.blit(borders, (tuple(rect)[0] - border_thickness,tuple(rect)[3] - border_thickness))
+        self._window.surface.blit(borders, (tuple(rect)[0] - border_thickness,tuple(rect)[1] - border_thickness))
         self._cam.start_preview(hflip=flip,
                                 fullscreen=False,
                                 window=tuple(rect))
