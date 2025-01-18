67980c7925916bfd6cc5ec7343a7ab4040a54baf
jsaidler <jsaidler@gmail.com>
2025-01-18 03:55:21 -0300

    Update rpi.py

--- pibooth/camera/rpi.py
+++ pibooth/camera/rpi.py
@@ -31,8 +31,8 @@ def get_rpi_camera_proxy(port=None):
         stdout, _stderr = process.communicate()
         if stdout and u'detected=1' in stdout.decode('utf-8'):
             if port is not None:
-                return picamera.PiCamera(camera_num=port, sensor_mode=2)
-            return picamera.PiCamera(sensor_mode=2)
+                return picamera.PiCamera(camera_num=port)
+            return picamera.PiCamera()
     except OSError:
         pass
     return None
@@ -63,10 +63,6 @@ class RpiCamera(BaseCamera):
         self._cam.meter_mode = 'matrix'
         self._cam.sharpness = 33
         self._cam.still_stats = True
-        # self._cam.zoom = ((1.0 - self.resolution[0] / self._cam.MAX_RESOLUTION[0]) / 2,
-        #                   (1.0 - self.resolution[1] / self._cam.MAX_RESOLUTION[1]) / 2,
-        #                   self.resolution[0] / self._cam.MAX_RESOLUTION[0],
-        #                   self.resolution[1] / self._cam.MAX_RESOLUTION[1]) #proporção 4x6
         self._shutter_values = np.array([15, 30, 60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840, 900, 960, 1020])
         self._iso_values = np.array([100, 200, 320, 400, 640, 800])
 
@@ -86,6 +82,7 @@ class RpiCamera(BaseCamera):
         """
         rect = self._window.get_rect(absolute=True)
         size = (rect.width - 2 * self._border, rect.height - 2 * self._border)
+        LOGGER.info("Imagem: %s, Tela: %s",self.resolution, size)
         if max_size:
             size = (min(size[0], max_size[0]), min(size[1], max_size[1]))
         res = sizing.new_size_keep_aspect_ratio(self.resolution, size)
