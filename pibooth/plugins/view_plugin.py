4621af9ac27d6029a3016a989c8e2420fbffa141
jsaidler <jsaidler@gmail.com>
2025-01-19 02:59:48 -0300

    Update view_plugin.py

--- pibooth/plugins/view_plugin.py
+++ pibooth/plugins/view_plugin.py
@@ -39,7 +39,7 @@ class ViewPlugin(object):
         LOGGER.info("Access state_wait_enter")
         win.show_intro(app.previous_picture, app.printer.is_ready())
         win.set_print_number(len(app.printer.get_all_tasks()), app.printer.is_ready())
-        app.camera.stop_preview()
+        # app.camera.stop_preview()
 
     @pibooth.hookimpl
     def state_wait_do(self, app, win, events):
