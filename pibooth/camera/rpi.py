584b4efeb6c8c909300793818df364e1c5290493
jsaidler <jsaidler@gmail.com>
2025-01-19 11:46:46 -0300

    Update rpi.py

--- pibooth/camera/rpi.py
+++ pibooth/camera/rpi.py
@@ -64,7 +64,7 @@ class RpiCamera(BaseCamera):
         self._cam.meter_mode = 'matrix'
         self._cam.sharpness = 33
         self._cam.still_stats = True
-        self._shutter_values = np.array([15, 30, 60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840, 900, 960, 1020])
+        self._shutter_values = np.array([15, 30, 60, 120, 240, 480, 960, 1920, 3840])
         self._iso_values = np.array([100, 200, 320, 400, 640, 800])
 
     def _post_process_capture(self, capture_data):
